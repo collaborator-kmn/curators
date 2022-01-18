3ab87b870cc26d181abe548fbc5bf5b3f1030e7b
collaborator-kmn <68353619+collaborator-kmn@users.noreply.github.com>
2022-01-18 17:30:37 +0300

    with button

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
                 <center>
                     <h1>Каталог кураторов клиентов</h1>
                 </center>
-                <CatalogList v-bind:catalog="catalog"/>
                 <CatalogTable v-bind:catalog="catalog"/>
             </v-container>
         </v-main>
@@ -15,28 +14,37 @@
 
 
 <script>
-import CatalogList from "@/components/CatalogList";
 import {Catalog} from "@/entity/Catalog";
 import CatalogTable from "@/components/CatalogTable";
 
 export default {
     name: 'App',
 
-    components: {CatalogTable, CatalogList},
+    components: {CatalogTable},
 
-    mounted() {
-        fetch('http://localhost:8081/catalog')
-            .then(response => response.json())
-            .then(json => this.catalog = json)
-    },
+    // mounted() {
+    //     fetch('http://localhost:8081/catalog')
+    //         .then(response => response.json())
+    //         .then(json => this.catalog = json)
+    // },
 
     data: () => ({
         catalog: [
+            {
+                arm: "internal_arm_1", boss: "internal_boss_1", curators: [
+                    "curator_1", "curator_2"
+                ]
+            },
+            {
+                arm: "internal_arm_2", boss: "internal_boss_2", curators: [
+                    "curator_4", "curator_3"
+                ]
+            }
         ]
     }),
 
-    computed:{
-        catalogs(){
+    computed: {
+        catalogs() {
             return this.catalog.map(result => new Catalog(result))
         }
     }
--- src/components/CatalogTable.vue
+++ src/components/CatalogTable.vue
@@ -1,18 +1,25 @@
 <template>
-    <div>
         <v-data-table
             :headers="headers"
             :items="catalog"
             item-key="name"
             class="elevation-1">
-            <template v-slot:body.append>
-                <tr>
-                    <td></td>
-                    <td colspan="4"></td>
-                </tr>
+            <template v-slot:[`item.curators`]="{ catalog }">
+                <v-btn
+                    class="ma-2"
+                    color="primary"
+                    dark
+                >
+                    Изменить
+                <v-icon
+                    small
+                    class="mr-2"
+                    @click.stop="$emit('edit', catalog)">
+                    mdi-pencil
+                </v-icon>
+                </v-btn>
             </template>
         </v-data-table>
-    </div>
 </template>
 
 <script>
